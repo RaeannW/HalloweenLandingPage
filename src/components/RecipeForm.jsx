cc067245c5c5a00f5710bd13d12c34eaa8c6a4a9
Rae Wiechens <raeannw321@gmail.com>
2025-08-22 20:57:34 -0500

    set up firebase db and modified form values to match db collection

--- src/components/RecipeForm.jsx
+++ src/components/RecipeForm.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase.config.js";
 
 export default function RecipeForm() {
   const [isFormOpen, setIsFormOpen] = useState(false);
@@ -11,58 +13,88 @@ export default function RecipeForm() {
     setIsFormOpen(false);
   };
 
+  const [form, setForm] = useState({
+    Title: "",
+    Category: "",
+    Description: "",
+    Ingredients: [],
+    Steps: [],
+    Name: "",
+  });
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await addDoc(collection(db, "recipes"), {
+      ...form,
+      createdAt: serverTimestamp(),
+    });
+    setForm({
+      Title: "",
+      Category: "",
+      Description: "",
+      Ingredients: [],
+      Steps: [],
+      Name: "",
+    });
+  };
+
   return (
     <>
-      {!isFormOpen && <button className="orangeButton" onClick={handleOpen}>Submit Recipe</button>}
+      {!isFormOpen && (
+        <button className="orangeButton" onClick={handleOpen}>
+          Submit Recipe
+        </button>
+      )}
 
       {isFormOpen && (
         <div id="recipeFormWrapper">
           <div className="formTop">
             <div className="formColOne">
-            <div className="circle"></div>
-            <div className="circle"></div>
-            <div className="circle"></div>
+              <div className="circle"></div>
+              <div className="circle"></div>
+              <div className="circle"></div>
             </div>
             <div className="formColTwo">
-            <button onClick={handleClose} className="closeFormButton">X</button>
+              <button onClick={handleClose} className="closeFormButton">
+                X
+              </button>
             </div>
           </div>
-          <form id="recipeForm">
-            <label htmlFor="recipeName">Recipe Title:</label>
-            <input type="text" id="recipeName" name="recipeName" />
+          <form id="recipeForm" onSubmit={handleSubmit}>
+            <label htmlFor="Title">Recipe Title:</label>
+            <input type="text" id="title" name="Title" onChange={handleChange} value={form.Title}/>
 
             <fieldset>
               <legend>Recipe Category:</legend>
 
-              <input type="radio" id="desert" name="category" value="desert" />
+              <input type="radio" id="desert" name="Category" value="desert" checked={form.Category === "desert"} onChange={(e) => setForm({ ...form, Category: e.target.value })} />
               <label htmlFor="desert">Desert</label>
               <br />
 
-              <input type="radio" id="drink" name="category" value="drink" />
+              <input type="radio" id="drink" name="Category" value="drink" checked={form.Category === "drink"} onChange={(e) => setForm({ ...form, Category: e.target.value })}/>
               <label htmlFor="drink">Drink</label>
               <br />
 
-              <input
-                type="radio"
-                id="appetizer"
-                name="category"
-                value="appetizer"
-              />
+              <input type="radio" id="appetizer" name="Category" value="appetizer" checked={form.Category === "appetizer"} onChange={(e) => setForm({ ...form, Category: e.target.value })} />
               <label htmlFor="appetizer">Appetizer</label>
               <br />
             </fieldset>
 
-            <label htmlFor="description">Description:</label>
-            <textarea id="description" name="description" rows="4"></textarea>
+            <label htmlFor="Description">Description:</label>
+            <textarea id="description" name="Description" rows="4" onChange={handleChange} value={form.Description}></textarea>
 
-            <label htmlFor="ingredients">Ingredients:</label>
-            <textarea id="ingredients" name="ingredients" rows="4"></textarea>
+            <label htmlFor="Ingredients">Ingredients:</label>
+            <textarea id="ingredients" name="Ingredients" rows="4" onChange={handleChange} value={form.Ingredients}></textarea>
 
-            <label htmlFor="instructions">Instructions:</label>
-            <textarea id="instructions" name="instructions" rows="8"></textarea>
+            <label htmlFor="Steps">Steps:</label>
+            <textarea id="steps" name="Steps" rows="8" onChange={handleChange} value={form.Steps}></textarea>
 
-            <label htmlFor="recipeOwner">Your Name:</label>
-            <input type="text" id="recipeOwner" name="recipeOwner" />
+            <label htmlFor="Name">Your Name:</label>
+            <input type="text" id="name" name="Name" onChange={handleChange} value={form.Name}/>
 
             <button
               className="wandButton"
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,34 @@
+import { collection, query, orderBy, limit, getDocs } from "firebase/firestore";
 import AnimatedText from "../components/AnimatedText";
 import EyeFollow from "../components/EyeFollow";
 import Nav from "../components/Nav";
+import { db } from "../firebase.config";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchLatestRecipes = async () => {
+      const q = query(
+        collection(db, "recipes"),
+        // orderBy("createdAt"),
+        limit(5)
+      );
+
+      const querySnapShot = await getDocs(q);
+      const recipeList = querySnapShot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+      setRecipes(recipeList);
+    };
+
+    fetchLatestRecipes();
+  }, [])
+  
+
+
   return (
     <>
       <main>
@@ -22,12 +48,15 @@ export default function Home() {
             </div>
             <div className="recipes" id="latestRecipes">
               <h2>Latest Recipes</h2>
-              <p>One</p>
+              {recipes.map(recipe => (
+                <li key={recipe.id}>{recipe.Title} - {recipe.Description} by: {recipe.Name}</li>
+              ))}
+              {/* <p>One</p>
               <p>Two</p>
               <p>Three</p>
               <p>Four</p>
               <p>Five</p>
-              <p>Six</p>
+              <p>Six</p> */}
             </div>
             <div className="cloudBackgroundTwo"></div>
             <div className="bottomHalf"></div>
